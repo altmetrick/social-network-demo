199c7d2ee6c5672938631f410af022c2801ff3a7
Altmertrick <altmertricks@gmail.com>
2022-06-13 14:24:03 +0300

    18 Refactored Users component as class component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,58 +1,52 @@
 import s from './Users.module.css';
 import userImage from './../../assets/images/User_default_avatar.png';
 
+import React from 'react';
+
 import axios from 'axios';
 
-let ss = {
-  name: 'Dicastes',
-  id: 24440,
-  uniqueUrlName: null,
-  photos: {
-    small: null,
-    large: null,
-  },
-  status: null,
-  followed: false,
-};
-
-const Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
-      .then((res) => {
-        props.setUsers(res.data.items);
-      });
+class Users extends React.Component {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
+      axios
+        .get('https://social-network.samuraijs.com/api/1.0/users')
+        .then((res) => {
+          this.props.setUsers(res.data.items);
+        });
+    }
   }
 
-  let usersEls = props.users.map((user) => (
-    <div key={user.id} className={s.userWrapper}>
+  render() {
+    let usersEls = this.props.users.map((user) => (
+      <div key={user.id} className={s.userWrapper}>
+        <div>
+          <div>
+            <img
+              src={user.photos.small === null ? userImage : user.photos.small}
+            />
+          </div>
+
+          <div>
+            <button onClick={() => this.props.toggleFollowed(user.id)}>
+              {user.followed ? 'Unfollow' : 'Follow'}
+            </button>
+          </div>
+        </div>
+
+        <div className={s.userBio}>
+          <h3>{user.name}</h3>
+          <h4>location: ---</h4>
+        </div>
+      </div>
+    ));
+
+    return (
       <div>
-        <div>
-          <img
-            src={user.photos.small === null ? userImage : user.photos.small}
-          />
-        </div>
-
-        <div>
-          <button onClick={() => props.toggleFollowed(user.id)}>
-            {user.followed ? 'Unfollow' : 'Follow'}
-          </button>
-        </div>
+        <h3>Users</h3>
+        {usersEls}
       </div>
-
-      <div className={s.userBio}>
-        <h3>{user.name}</h3>
-        <h4>location: ---</h4>
-      </div>
-    </div>
-  ));
-
-  return (
-    <div>
-      <h3>Users</h3>
-      {usersEls}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
